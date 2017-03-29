e5de0aa56f781409c6d5193b20eefdd311574b70
James <428811@cognizant.com>
2017-03-29 20:00:00 +0530

    add test file

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe("Add groupName", function() {
 it("returns status code 201", function(done) {
 request.get('http://localhost:3000/api/groups/sachin',function(error,response,body)
 {
-assert.equal(201,response.statusCode);
+assert.equal(201,response.status);
 app.closeServer();
 done();
 });
