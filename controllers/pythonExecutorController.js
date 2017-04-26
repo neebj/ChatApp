485c5a54ad0525063d0fd42be8a0f22d226e88b7
James <428811@cognizant.com>
2017-04-26 12:20:42 +0530

    NLP implementation

---
+++ controllers/pythonExecutorController.js
@@ -0,0 +1,32 @@
+(function (pythonExecutorController) {
+    
+    
+    pythonExecutorController.init = function (app) {
+      
+        app.get("/api/pythonexecutor/:inputMessage", function (req, res) {
+              console.log('python started');
+        var msgAfterNLP='';
+        inputMessage =req.params.inputMessage;
+        var PythonShell = require('python-shell');
+        var pyshell = new PythonShell('./controllers/compute_input.py');
+        pyshell.send(JSON.stringify(inputMessage));
+        pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+        msgAfterNLP=message;
+         
+        });
+
+        pyshell.end(function (err) {
+            if (err){
+                throw err;
+            };
+              console.log('python ended');
+              res.render("index", { Title: msgAfterNLP });
+        });
+
+
+            
+        });
+    }
+}
+)(module.exports);
--- controllers/starterController.js
+++ controllers/starterController.js
@@ -1,11 +1,14 @@
 (function (starterController) {
     var chatController = require("./chatController.js");
     var chatGroupController = require("./chatgroupController.js");
+    var pythonExecutorController = require("./pythonExecutorController.js");
 
     starterController.init=function(app)
     {
+        pythonExecutorController.init(app);
         chatController.init(app);
         chatGroupController.init(app);
+       
     }
 }
 )(module.exports);
