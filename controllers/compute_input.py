182e69b7f140d33170e51540571176805180314e
James <428811@cognizant.com>
2017-04-28 09:15:35 +0530

    Working NLP installation

--- controllers/compute_input.py
+++ controllers/compute_input.py
@@ -12,23 +12,22 @@ def read_in():
     return json.loads(lines[0])
 
 def main():
-    print("Hello World")
-    # #get our data as an array from read_in()
-    # from nltk.tokenize import word_tokenize
-    # from nltk.corpus import stopwords
-    # #from nltk.tag import StanfordNERTagger
-    # lines = read_in()
-    # tokens = word_tokenize(lines)
-    # stop_words = set(stopwords.words('english'))
-    # clean_tokens = [w for w in tokens if not w in stop_words]
-    # tagged = nltk.pos_tag(clean_tokens)
-    # # st = StanfordNERTagger('./controllers/english.all.3class.distsim.crf.ser.gz',
-	# # 				   './controllers/stanford-ner.jar',
-	# # 				   encoding='utf-8')
-    # # classified_text = st.tag(clean_tokens)
-
-    # # print(classified_text)                   
-    # print(nltk.ne_chunk(tagged))
+    
+    #get our data as an array from read_in()
+    from nltk.tokenize import word_tokenize
+    from nltk.corpus import stopwords
+    #from nltk.tag import StanfordNERTagger
+    lines = read_in()
+    tokens = word_tokenize(lines)
+    stop_words = set(stopwords.words('english'))
+    clean_tokens = [w for w in tokens if not w in stop_words]
+    tagged = nltk.pos_tag(clean_tokens)
+    # st = StanfordNERTagger('./controllers/english.all.3class.distsim.crf.ser.gz',
+	# 				   './controllers/stanford-ner.jar',
+	# 				   encoding='utf-8')
+    # classified_text = st.tag(clean_tokens)
+    # print(classified_text)                   
+    print(nltk.ne_chunk(tagged))
    
 # Start process
 if __name__ == '__main__':
